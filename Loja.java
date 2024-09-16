7f9ad102430beea8ccb8659c0daa9a42c96e7272
egroehs <eduardagroehs@gmail.com>
2024-09-16 11:54:42 -0700

    Inicial commit

---
+++ Loja.java
@@ -0,0 +1,54 @@
+
+public class Loja {
+    private String nome;
+    private int quantidadeFuncionarios;
+    private double salarioBaseFuncionario;
+
+    //Construtor 1
+    public Loja(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario) {
+        this.nome = nome;
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+        this.salarioBaseFuncionario = salarioBaseFuncionario;
+
+    }
+
+    //Construtor 2
+    public Loja(String nome, int quantidadeFuncionarios) {
+        this.nome = nome;
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+        this.salarioBaseFuncionario = -1;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidadeFuncionarios() {
+        return quantidadeFuncionarios;
+    }
+
+    public void setQuantidadeFuncionarios(int quantidadeFuncionarios) {
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+    }
+
+    public double getSalarioBaseFuncionario() {
+        return salarioBaseFuncionario;
+    }
+
+    public void setSalarioBaseFuncionario(double salarioBaseFuncionario) {
+        this.salarioBaseFuncionario = salarioBaseFuncionario;
+    }
+
+    @Override
+    public String toString() {
+        return "Loja: " + nome + ", Funcionários: " + quantidadeFuncionarios + ", Salário Base: " + salarioBaseFuncionario;
+    }
+
+
+}
+
+
---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Loja loja1 = new Loja("Supermercado XYZ", 50, 2000.00);
+        Loja loja2 = new Loja("Padaria ABC", 10);
+
+        System.out.println(loja1);
+        System.out.println(loja2);
+    }
+}
